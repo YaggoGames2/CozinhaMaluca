47d9d7376bcb8b6b4e43be141c47b05fa46680eb
YAGGO ROSA DE ALMEIDA SILVA <yaggo.rasilva@senacsp.edu.br>
2025-02-11 17:31:35 -0300

    Aula 11/2

---
+++ Assets/Scripts/ControladorJogador.cs
@@ -0,0 +1,48 @@
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ControladorJogador : MonoBehaviour
+{
+    public float taxaMovimentacao;
+    public Geral juizDoJogo;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        float altX, altY;
+
+        // Para cima e para baixo - mexe no Y
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < 467.6)
+            altY = 60 * Time.deltaTime * taxaMovimentacao;
+        else if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > 32.2)
+            altY = -60 * Time.deltaTime * taxaMovimentacao;
+        else
+            altY = 0;
+
+        // Para os lados
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > 31.9)
+            altX = -60 * Time.deltaTime * taxaMovimentacao;
+        else if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 927.8)
+            altX = 60 * Time.deltaTime * taxaMovimentacao;
+        else
+            altX = 0;
+
+        // Modificar posição:
+
+        Vector3 novaPos = new Vector3(altX, altY, 0);
+        transform.position += novaPos;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Voador")
+        {
+            juizDoJogo.MarcarPonto();
+            collision.GetComponent<ControladorVoador>().posicaoObj.x =
+                collision.GetComponent<ControladorVoador>().posInicialX;
+        }
+    }
+}
